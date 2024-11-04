c791fb250fa339e7e23dd043761b549551383b7e
RaphaelDGYT <triocraftextreme@gmail.com>
2024-11-04 12:12:49 -0300

    feat: Adicionando conectores DB, função valida_usuario e gitignore

--- config.py
+++ config.py
@@ -1,2 +1,33 @@
+from dotenv import load_dotenv
+import mysql.connector as mysql
+import os
 
+load_dotenv()  # Carrega as variáveis do arquivo .env
 
+# Acessando as variáveis
+host = os.getenv("MYSQLHOST")
+user = os.getenv("MYSQLUSER")
+password = os.getenv("MYSQLPASSWORD")
+database = os.getenv("MYSQLDATABASE")
+port = os.getenv("MYSQLPORT")
+
+class DB():
+    def __init__(self):
+        try:
+            self.conn = mysql.connect(
+                host = host,
+                user = user,
+                password = password,
+                database = database,
+                port = port)
+            self.cursor = self.conn.cursor()
+        except mysql.Error as error:
+            return "Erro ao conectar ao banco de dados", error
+        
+    def conexao_db(self):
+        return self.cursor
+    
+    def fechar_conexao(self):
+        if self.conn.is_connected():
+            self.cursor.close()
+            self.conn.close()
