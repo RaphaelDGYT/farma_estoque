283dbe8ba64ef24a7ba23c4daf0612024743f9f3
RaphaelDGYT <triocraftextreme@gmail.com>
2024-11-14 23:53:05 -0300

    feat: adicionando filtros de estoque por quantidade

---
+++ model/estoque_filtro.py
@@ -0,0 +1,43 @@
+import os
+import sys  
+import pandas as pd
+import datetime as datetime
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from config import DB
+from relatorio import Relatorio
+
+class Estoque(Relatorio):
+    def __init__(self):
+        super().__init__()
+
+    def estoque_intervalo(self, ini, fim):
+        tabela = super().relatorio()
+        tabela_filtrada = tabela[(tabela["Estoque"] >= ini) & (tabela["Estoque"] <= fim)]
+        if tabela_filtrada.empty:
+            return print("Nenhum medicamento encontrado!")
+        else:
+            return tabela_filtrada
+        
+    def estoque_menor_que(self, quantidade):
+        tabela = super().relatorio()
+        tabela_filtrada = tabela[tabela["Estoque"] < quantidade]
+        if tabela_filtrada.empty:
+            return print("Medicamento não encontrado!")
+        else:
+            tabela_filtrada
+
+    def estoque_maior_que(self, quantidade):
+        tabela = super().relatorio()
+        tabela_filtrada = tabela[tabela["Estoque"] > quantidade]
+        if tabela_filtrada.empty:
+            return print("Medicamento não encontrado!")
+        else:
+            return tabela_filtrada
+        
+    def estoque_igual(self, quantidade):
+        tabela = super().relatorio()
+        tabela_filtrada = tabela[tabela["Estoque"] == quantidade]
+        if tabela_filtrada.empty:
+            return print("Medicamento não encontrado!")
+        else:
+            return tabela_filtrada
